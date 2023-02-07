6cb0ff8a704234312f9e58bd6e2b412c5c3a0036
Julia Sena <juliasenas@gmail.com>
2023-02-07 14:56:20 -0300

    atualização das mensagens

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -11,37 +11,43 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   const userList = await listUsers();
-  res.send(userList);
+  res.send({
+    message: `Existem ${userList.length} usuários na lista`,
+    users: userList,
+  });
 });
 
 router.post("/", async (req, res) => {
   try {
     const user = await createUser(req.body);
-    res.status(201).send(user);
+    res.status(201).send({ message: "Usuário cadastrado com sucesso.", user });
   } catch (error) {
-    res.status(400).send(error);
+    if (error.code === 11000) {
+      return res.status(400).send({ message: "Email já está cadastrado." });
+    }
+    return res.status(500).send(error);
   }
 });
 
 router.delete("/:userId", async (req, res) => {
   await deleteUser(req.params.userId);
-  res.send();
+  res.send("Usuário excluído");
 });
 
 router.put("/:userId", async (req, res) => {
   await updateUser(req.params.userId, req.body);
-  res.send();
+  res.send("Dados atualizados");
 });
 
 const authenticateUser = async (name, password) => {
   const user = await User.findOne({ name });
   if (!user) {
-    throw new Error("usuário não encontrado");
+    throw new Error("Nome de usuário não encontrado");
   }
 
   const isPasswordMatch = await bcrypt.compare(password, user.password);
   if (!isPasswordMatch) {
-    throw new Error("senha incorreta");
+    throw new Error("Senha incorreta");
   }
 
   return user;
